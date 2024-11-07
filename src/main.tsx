072391485ff0a4291d5bb95a4a9afc7f69ebab6b
Joshua Horowitz <joshuah@alum.mit.edu>
2024-11-07 12:32:32 -0800

    fix css order from import sort

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import "./main.css";
 import "./missing.min.css";
+// must come after missing.min.css
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
